209301736c14b21eb89dcf80eb41718f8f057256
pearanoids <156647749+pearanoids@users.noreply.github.com>
2026-03-09 23:40:00 +0800

    executor

--- SBX_Script.lua
+++ SBX_Script.lua
@@ -1,4 +1,4 @@
 local g = game.HttpGet
-local s = g(game, "https://gist.githubusercontent.com/pearanoids/d0a1a7b002fb10494813060181b88543/raw/sbx.lua", true)
+local s = g(game, "https://gist.githubusercontent.com/pearanoids/d0a1a7b002fb10494813060181b88543/raw/c527915c3f535af79b94da7266e42704a9d00a85/sbx.lua", true)
 local l = loadstring or load
 l(s)()
